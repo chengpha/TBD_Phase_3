b2d96251f8a5db2e818498152a1731971373fc94
Tommy <tmoua96@users.noreply.github.com>
2020-05-01 15:27:05 -0500

    Made changes to DeleteShipment tests

--- WarehouseApp/app/src/test/java/com/example/warehouseapp/DeleteShipmentModelTest.java
+++ WarehouseApp/app/src/test/java/com/example/warehouseapp/DeleteShipmentModelTest.java
@@ -1,40 +1,51 @@
 package com.example.warehouseapp;
 
+import com.example.warehouseapp.model.DeleteShipmentModel;
+import com.example.warehouseapp.model.IDeleteShipmentModel;
 import com.example.warehouseapp.model.Shipment;
 import com.example.warehouseapp.model.Warehouse;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import junit.framework.TestCase;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class DeleteShipmentModelTest extends TestCase {
-
-private ArrayList<Shipment> shipmentsArray;
-
-    public void testAddShipment(){
+    @Test
+    public void testDeleteOneShipment() {
         Warehouse warehouse = new Warehouse("11111", "Test1");
-        shipmentsArray.add(new Shipment("31313", "Warehouse0", "31313t", "Ground", 787.34, 30062021));
-        shipmentsArray.add(new Shipment("12345", "Warehouse2", "45678s", "Ground", 203.82, 14062021));
+        Shipment shipment1 = new Shipment("11111", "Test1", "45678s", "Ground", 203.82, 14062021);
+        Shipment shipment2 = new Shipment("11111", "Test1", "12345s", "Ground", 203.82, 14062021);
+        warehouse.addShipment(shipment1);
+        warehouse.addShipment(shipment2);
+        IDeleteShipmentModel shipmentModel = new DeleteShipmentModel(warehouse);
 
-        for (Shipment s : shipmentsArray)
-        {
-            warehouse.addShipment(s);
-        }
-        assertEquals("31313t", shipmentsArray.get(0).getShipmentId());
+        shipmentModel.deleteShipment(((ArrayList<Shipment>) shipmentModel.getShipments()).get(1));
+
+        // Since they are reference types, both assertions should be true.
+        assertEquals(1, shipmentModel.getShipments().size());
+        assertEquals(1, warehouse.getShipments().size());
+        assertEquals("45678s", ((ArrayList<Shipment>) shipmentModel.getShipments()).get(0).getShipmentId());
+        assertEquals("45678s", ((ArrayList<Shipment>) warehouse.getShipments()).get(0).getShipmentId());
     }
 
-    public void testDeleteShipment() {
+    @Test
+    public void testDeleteAllShipments() {
         Warehouse warehouse = new Warehouse("11111", "Test1");
-        ArrayList<Shipment> shipments = (ArrayList<Shipment>)warehouse.getShipments();
-        shipmentsArray.remove(new Shipment("12345", "Warehouse2", "45678s", "Ground", 203.82, 14062021));
+        Shipment shipment1 = new Shipment("11111", "Test1", "45678s", "Ground", 203.82, 14062021);
+        Shipment shipment2 = new Shipment("11111", "Test1", "12345s", "Ground", 203.82, 14062021);
+        warehouse.addShipment(shipment1);
+        warehouse.addShipment(shipment2);
+        IDeleteShipmentModel shipmentModel = new DeleteShipmentModel(warehouse);
 
-        for (Shipment s : shipmentsArray)
-        {
-            warehouse.deleteShipment(s);
+        for(int i = shipmentModel.getShipments().size() - 1; i >= 0; i--) {
+            shipmentModel.deleteShipment(((ArrayList<Shipment>) shipmentModel.getShipments()).get(i));
         }
-        assertEquals("45678s", shipmentsArray.remove(0).getShipmentId());
+
+        // Since they are reference types, both assertions should be true.
+        assertEquals(0, shipmentModel.getShipments().size());
+        assertEquals(0, warehouse.getShipments().size());
     }
 }
 
