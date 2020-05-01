4e6bff4310b5bb2631a2124724832480f6e76f58
Tommy <tmoua96@users.noreply.github.com>
2020-05-01 15:27:05 -0500

    Added a test for the MainActivity and added some dependencies for testing.

--- WarehouseApp/app/src/main/java/com/example/warehouseapp/model/IDeleteShipmentModel.java
+++ WarehouseApp/app/src/main/java/com/example/warehouseapp/model/IDeleteShipmentModel.java
@@ -1,5 +1,8 @@
 package com.example.warehouseapp.model;
 
+import java.util.Collection;
+
 public interface IDeleteShipmentModel {
     void deleteShipment(Shipment shipment);
+    Collection<Shipment> getShipments();
 }
